f5a02640e6d038904b0928d77542b69e3f9efd44
Svetlana Ignatchik <lightbug89@gmail.com>
2022-12-28 23:52:44 +0300

    Task023

---
+++ Task023/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125 
+
+Console.Clear();
+
+int getUserValue(string message)
+{
+  Console.WriteLine(message);
+  int result = int.Parse(Console.ReadLine());
+  return result;
+}
+
+void displaySqrtTable(int number)
+
+{
+for (int index = 1; index < number +1; index++)
+ {
+ int result =  index*index*index;
+ if (index<number)
+  {
+  Console.Write($"{result},");
+  }  
+else 
+  {
+  Console.Write($"{result}.");
+  }
+  }
+
+}
+ 
+int number = getUserValue ($"Введите число");
+displaySqrtTable(number);
